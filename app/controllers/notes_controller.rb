671f27faaed9b1baf9fdee13593eb4247373ccff
Danieal <i.danieal@harukaedu.com>
2017-12-12 14:56:41 +0700

    create notes controller method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,96 @@
 class NotesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true
+  before_action :find_note, :only => [:update, :show, :delete]
+  before_action :note_params, :only => [:create, :update]
+
+  def create
+    if check_unique_name_by_user_id
+      render_409
+    else
+      @note = Note.new(@note_param)
+      if @note.save
+        render_data
+      end
+    end
+  end
+
+  def update
+    if @pad_id == @note["pad_id"] && @user_id == @note["user_id"] && @name == @note['name'] && @text == @note['text']
+      render_data
+    elsif check_unique_name_by_user_id
+      render_409
+    else
+      if @note.update(@note_param)
+        render_data
+      end
+    end
+  end
+
+  def show
+    render_data
+  end
+
+  def delete
+    @note.destroy
+    render status: 200, json: {
+      data: "null"
+    }.to_json
+  end
+
+  def index
+    @note = Note.all.select('id', 'pad_id', 'user_id', 'name', 'text')
+    render status: 200, json: {
+      data: @note
+    }.to_json
+  end
+
+  private
+  def note_params
+    @note_param = params.require("note").permit("pad_id", "user_id", "name", "text")
+    @pad_id = @note_param['pad_id']
+    @user_id = @note_param['user_id']
+    @name = @note_param['name']
+    @text = @note_param['text']
+    if (@pad_id.present? == false) || (@user_id.present? == false) || (@name.present? == false) || (@text.present? == false)
+      render  status: 400, json: {
+        message: "any required attribute is not present"
+      }.to_json
+    end
+  end
+
+  def find_note
+    begin
+      @note = Note.find params[:id]
+    rescue ActiveRecord::RecordNotFound => e
+      @note = nil
+    end
+    if @note == nil
+      render  status: 404, json: {
+        message: "The desired note_id is not found"
+      }.to_json
+    end
+  end
+
+  def render_data
+    render status: 200, json: {
+      data: [
+        id: @note['id'],
+        pad_id: @note['pad_id'],
+        user_id: @note['user_id'],
+        name: @note['name'],
+        text: @note['text']
+      ]
+    }.to_json
+  end
+
+  def check_unique_name_by_user_id
+    Note.find_by(user_id: @user_id, name: @name)
+  end
+
+  def render_409
+    render status: 409, json: {
+      message: "The desired name is already in use by the user_id"
+    }.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  '/pads', to: "pads#index"
-  post '/pads/create', to: "pads#create"
-  put  '/pads/:id', to: "pads#update"
-  get  '/pads/:id', to: "pads#show"
-  delete '/pads/:id', to: "pads#delete"
+  get     '/pads',        to: "pads#index"
+  post    '/pads/create', to: "pads#create"
+  put     '/pads/:id',    to: "pads#update"
+  get     '/pads/:id',    to: "pads#show"
+  delete  '/pads/:id',    to: "pads#delete"
+  get     '/notes',        to: "notes#index"
+  post    '/notes/create', to: "notes#create"
+  put     '/notes/:id',    to: "notes#update"
+  get     '/notes/:id',    to: "notes#show"
+  delete  '/notes/:id',    to: "notes#delete"
 end
